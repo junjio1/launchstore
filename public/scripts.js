8faf92c91f3ef4eda37018081d25597a510098ec
Junji Oshiro <junjio1@hotmail.com>
2022-01-05 11:28:02 -0300

    add logic to show images on html

--- public/scripts.js
+++ public/scripts.js
@@ -34,3 +34,38 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event) { 
+        const { files: fileList } = event.target
+        const { uploadLimit } = PhotosUpload
+
+        if(fileList.length > uploadLimit){
+            alert(`Envie no maximo ${uploadLimit} photos`)
+            event.preventDefault()
+            return
+        }
+
+        Array.from(fileList).forEach((file)=>{
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = document.createElement('div')
+                div.classList.add("photo")
+
+                div.onclick = () => alert('remover foto')
+
+                div.appendChild(image)
+
+                document.querySelector('#photos-preview').appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+
+        })
+    }
+}
