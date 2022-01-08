dbf2e98b2c775515c04d57684e02765bfd259de4
Junji Oshiro <junjio1@hotmail.com>
2022-01-08 08:20:00 -0300

    html  show.njk

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -41,6 +41,9 @@ async post(req, res){
 
     return res.redirect(`products/${productId}/edit`)
 },
+show(req, res){
+    return res.render("products/show.njk")
+},
 async edit(req, res){
  
     let results = await Product.find(req.params.id)
